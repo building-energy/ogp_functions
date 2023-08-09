cff2426fe84b468bdc3f5b9a2531f90019737698
Steven Firth <s.k.firth@lboro.ac.uk>
2023-08-09 21:55:25 +0100

    updates

--- ogp_functions/ogp_functions.py
+++ ogp_functions/ogp_functions.py
@@ -9,16 +9,546 @@ import urllib.request
 import json
 import os
 import zipfile
+import sqlite3
+import importlib.resources as pkg_resources
+import ogp_functions
+import subprocess
+from datetime import datetime
+import pandas as pd
+import csv
 
-
-#%% data folder
 
 _default_data_folder='_data'  # the default
 
 
+def load_data(
+        data_folder=_default_data_folder
+        ):
+    """
+    """
+    d={}
+    
+    # Changes.csv
+    
+    fp=os.path.join(data_folder,'Changes.csv')
+    
+    with open(fp,encoding='utf-8-sig') as f:
+        
+        csvreader=csv.DictReader(f)
+        
+        for row in csvreader:
+            
+            geogcd=row['GEOGCD']
+            geogcd_p=row['GEOGCD_P']
+            
+            x=d.setdefault(geogcd,{})
+            y=x.setdefault('changes',[])
+            y.append(row)
+            
+            x=d.setdefault(geogcd_p,{})
+            y=x.setdefault('__next__',[])
+            y.append(geogcd)
+    
+    # ChangeHistory.csv
+    
+    fp=os.path.join(data_folder,'ChangeHistory.csv')
+    
+    with open(fp,encoding='utf-8-sig') as f:
+        
+        csvreader=csv.DictReader(f)
+        
+        for row in csvreader:
+            
+            geogcd=row['GEOGCD']
+            parentcd=row['PARENTCD']
+            
+            x=d.setdefault(geogcd,{})
+            y=x.setdefault('changehistory',[])
+            y.append(row)
+            
+            x=d.setdefault(parentcd,{})
+            y=x.setdefault('__children__',[])
+            y.append(geogcd)
+    
+    
+    return d
+    
+
+
+def get_previous_codes(code,data):
+    ""
+    return [x['GEOGCD_P'] for x in data[code]['changes']]
+
+
+def get_next_codes(code,data):
+    ""
+    return data[code].get('__next__',[])
+
+
+def get_latest_codes(code,data):
+    """
+    """
+    result=set()
+    
+    def _get_next_codes(
+            result,
+            codes,
+            data):
+        
+        for code in codes:
+            
+            x=get_next_codes(code,data)  # a list of codes
+            
+            result.update(x)
+            
+            result = _get_next_codes(result,x,data)
+            
+        return result
+    
+    result =  _get_next_codes(result,[code],data)
+    
+    return list(result)
+
+
+def get_parent_codes(
+        code,
+        data
+        ):
+    """
+    """
+    return [x['PARENTCD'] for x in data[code]['changehistory']
+            if x['PARENTCD']!='']
+
+
+def get_child_codes(code,data):
+    ""
+    return data[code].get('__children__',[])
+
+def get_ancestor_codes(
+        code,
+        data
+        ):
+    """
+    """
+    result=set()
+    
+    def _get_parent_codes(
+            result,
+            codes,
+            data
+            ):
+        
+        for code in codes:
+            
+            x=get_parent_codes(code,data)  # a list of codes
+            
+            result.update(x)
+            
+            result = _get_parent_codes(result,x,data)
+            
+        return result
+    
+    result =  _get_parent_codes(result,[code],data)
+    
+    return list(result)
+
+
+def get_descendent_codes(
+        code,
+        data
+        ):
+    """
+    """
+    result=set()
+    
+    def _get_child_codes(
+            result,
+            codes,
+            status):
+        
+        for code in codes:
+            
+            x=get_child_codes(code,data)  # a list of codes
+                            
+            result.update(x)
+            
+            result = _get_child_codes(result,x,data)
+            
+        return result
+    
+    result =  _get_child_codes(result,[code],data)
+    
+    return list(result)
+
+
+
+
+#%% read metadata files
+
+# Changes-schema-metadata.json
+fp=os.path.join(
+    pkg_resources.files(ogp_functions),
+    'Changes-schema-metadata.json'
+    )
+with open(fp) as f:
+    changes_schema_metadata=json.load(f)
+
+# ChangeHistory-schema-metadata.json
+fp=os.path.join(
+    pkg_resources.files(ogp_functions),
+    'ChangeHistory-schema-metadata.json'
+    )
+with open(fp) as f:
+    changehistory_schema_metadata=json.load(f)
+
+
+
+class OGPdata():
+    """
+    """
+    
+    def __init__(
+            self,
+            data_folder='_data'
+        ):
+        """
+        """
+        self.data_folder=data_folder
+        
+        self.df_changes=self._load_changes_dataframe()
+        #print(self._df_changes)
+        
+        self.df_changehistory=self._load_changehistory_dataframe()
+        
+        
+        
+        
+
+    def _load_changes_dataframe(self):
+        ""
+        fp=os.path.join(self.data_folder,'Changes.csv')
+        return pd.read_csv(fp)
+        
+    
+    def _load_changehistory_dataframe(self):
+        ""
+        fp=os.path.join(self.data_folder,'ChangeHistory.csv')
+        return pd.read_csv(fp)
+
+
+    def get_previous_codes(self,code):
+        """Returns the previous codes of a code change.
+        """
+        
+        return self.df_changes[self.df_changes.GEOGCD==code].GEOGCD_P.dropna().to_list()
+    
+    def get_next_codes(self,code):
+        """
+        """
+        
+        return self.df_changes[self.df_changes.GEOGCD_P==code].GEOGCD.dropna().to_list()
+        
+        
+    def get_latest_codes(self,code):
+        """
+        """
+        result=set()
+        
+        def _get_next_codes(
+                result,
+                codes):
+            
+            for code in codes:
+                
+                x=self.get_next_codes(code)  # a list of codes
+                
+                result.update(x)
+                
+                result = _get_next_codes(result,x)
+                
+            return result
+        
+        result =  _get_next_codes(result,[code])
+        
+        return list(result)
+    
+    
+    def get_parent_codes(
+            self,
+            code,
+            status='live'  # 'live' or 'terminated' or none 
+            ):
+        """
+        """
+        
+        if status is None:
+            
+            mask = self.df_changehistory.GEOGCD==code
+            
+        else:
+            
+            mask = (self.df_changehistory.GEOGCD==code) & (self.df_changehistory.STATUS==status)
+            
+        result = self.df_changehistory[mask].PARENTCD.dropna().to_list()
+            
+        return result
+    
+    
+    def get_ancestor_codes(
+            self,
+            code,
+            status='live'  # 'live' or 'terminated' or none 
+            ):
+        """
+        """
+        result=set()
+        
+        def _get_parent_codes(
+                result,
+                codes,
+                status):
+            
+            for code in codes:
+                
+                x=self.get_parent_codes(code,status)  # a list of codes
+                
+                result.update(x)
+                
+                result = _get_parent_codes(result,x,status)
+                
+            return result
+        
+        result =  _get_parent_codes(result,[code],status)
+        
+        return list(result)
+       
+        
+    def get_child_codes(
+            self,
+            code,
+            status='live'  # 'live' or 'terminated' or none 
+            ):
+        """
+        """
+      
+        if status is None:
+            
+            mask = self.df_changehistory.PARENTCD==code
+            
+        else:
+            
+            mask = (self.df_changehistory.PARENTCD==code) & (self.df_changehistory.STATUS==status)
+            
+        result = self.df_changehistory[mask].GEOGCD.dropna().to_list()
+            
+        return result
+       
+
+    def get_descendent_codes(
+            self,
+            code,
+            status='live'  # 'live' or 'terminated' or none 
+            ):
+        """
+        """
+        result=set()
+        
+        def _get_child_codes(
+                result,
+                codes,
+                status):
+            
+            for code in codes:
+                
+                if code[1:3]=='00':
+                    continue
+                
+                x=self.get_child_codes(code,status)  # a list of codes
+                                
+                result.update(x)
+                
+                result = _get_child_codes(result,x,status)
+                
+            return result
+        
+        result =  _get_child_codes(result,[code],status)
+        
+        return list(result)
+
+
+
+#%% database functions
+
+def _get_fp_database():
+    ""
+    return os.path.join(_default_data_folder,'ogp.sqlite')
+
+
+def _check_if_table_exists_in_database(
+        fp_database,
+        table_name
+        ):
+    ""
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        query=f"SELECT count(*) FROM sqlite_master WHERE type='table' AND name='{table_name}';"
+        return True if c.execute(query).fetchall()[0][0] else False
+    
+
+
+def _create_changes_table(
+        fp_database,
+        verbose=True
+        ):
+    """Creates a changes table in the sqlite database.
+    
+    Replaces any existing 'changes' table.
+    
+    """
+    
+    # drop table in database
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        query='DROP TABLE IF EXISTS changes;'
+        c.execute(query)
+        conn.commit()
+    
+    # create query
+    datatype_map={
+    'integer':'INTEGER',
+    'decimal':'REAL'
+    }
+    query='CREATE TABLE changes ('
+    for column_dict in changes_schema_metadata['columns']:
+        name=column_dict['name']
+        datatype=datatype_map.get(column_dict['datatype']['base'],'TEXT')
+        query+=f"{name} {datatype}"
+        query+=", "
+    query=query[:-2]
+    query+=');'
+    
+    if verbose:
+        print('---QUERY TO CREATE TABLE---')
+        print(query)
+    
+    # create empty table in database
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        c.execute(query)
+        conn.commit()
+        
+    # create indexes
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        query="CREATE INDEX index_changes_GEOGCD ON changes(GEOGCD)"
+        c.execute(query)
+        query="CREATE INDEX index_changes_GEOGCD_P ON changes(GEOGCD_P)"
+        c.execute(query)
+        conn.commit()
+        
+    # import data into table
+    fp_database2=fp_database.replace('\\','\\\\')
+    fp_csv=os.path.join(_default_data_folder,'Changes.csv')
+    fp_csv2=fp_csv.replace('\\','\\\\')
+    command=f'sqlite3 {fp_database2} -cmd ".mode csv" ".import --skip 1 {fp_csv2} changes"'
+    if verbose:
+        print('---COMMAND LINE TO IMPORT DATA---')
+        print(command)
+    subprocess.run(command)
+    if verbose:
+        print('Number of rows after import: ', _get_row_count_in_database_table(fp_database,'changes'))
+    
+    
+def _create_changehistory_table(
+        fp_database,
+        verbose=True
+        ):
+    """Creates a changehistory table in the sqlite database.
+    
+    Replaces any existing 'changehistory' table.
+    
+    """
+    
+    # drop table in database
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        query='DROP TABLE IF EXISTS changehistory;'
+        c.execute(query)
+        conn.commit()
+    
+    # create query
+    datatype_map={
+    'integer':'INTEGER',
+    'decimal':'REAL'
+    }
+    query='CREATE TABLE changehistory ('
+    for column_dict in changehistory_schema_metadata['columns']:
+        name=column_dict['name']
+        datatype=datatype_map.get(column_dict['datatype']['base'],'TEXT')
+        query+=f"{name} {datatype}"
+        query+=", "
+    query=query[:-2]
+    query+=');'
+    
+    if verbose:
+        print('---QUERY TO CREATE TABLE---')
+        print(query)
+    
+    # create empty table in database
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        c.execute(query)
+        conn.commit()
+        
+    # create indexes
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        query="CREATE INDEX index_changehistory_GEOGCD ON changehistory(GEOGCD)"
+        c.execute(query)
+        query="CREATE INDEX index_changehistory_PARENTCD ON changehistory(PARENTCD)"
+        c.execute(query)
+        query="CREATE INDEX index_changehistory_STATUS ON changehistory(STATUS)"
+        c.execute(query)
+        conn.commit()
+        
+    # import data into table
+    fp_database2=fp_database.replace('\\','\\\\')
+    fp_csv=os.path.join(_default_data_folder,'ChangeHistory.csv')
+    fp_csv2=fp_csv.replace('\\','\\\\')
+    command=f'sqlite3 {fp_database2} -cmd ".mode csv" ".import --skip 1 {fp_csv2} changehistory"'
+    if verbose:
+        print('---COMMAND LINE TO IMPORT DATA---')
+        print(command)
+    subprocess.run(command)
+    if verbose:
+        print('Number of rows after import: ', _get_row_count_in_database_table(fp_database,'changehistory'))
+    
+    
+    
+        
+def _get_row_count_in_database_table(
+        fp_database,
+        table_name,
+        column_name='*'
+        ):
+    """Gets number of rows in table
+    
+    """
+    with sqlite3.connect(fp_database) as conn:
+        c = conn.cursor()
+        query=f'SELECT COUNT({column_name}) FROM {table_name}'
+        return c.execute(query).fetchone()[0]
+    
+
+
+
+#%% data folder
+
+
+
 def get_latest_data_file_info():
     """
-    #%% download the latest data file information from GitHub
+    # download the latest data file information from GitHub
 
     # - the file on GitHub can be updated as new data files are released
     #   on the Open Geography Portal website.
@@ -53,7 +583,7 @@ def download_latest_data_files(
         
         json.dump(latest_data_file_info,f,indent=4)
         
-    # download files
+    # # download files
     for file_info in latest_data_file_info:
         
         download_url = file_info['download_url']  # str
@@ -92,18 +622,191 @@ def download_latest_data_files(
                 filename=data_file
                 )
             
+    # create changes table in database
+    _create_changes_table(
+        fp_database=_get_fp_database()
+        )
+    
+    # create changehistory table in database
+    _create_changehistory_table(
+        fp_database=_get_fp_database()
+        )
+
+    
     
 #%% main functions
 
-def get_replacement_code(code):
-    """
-    """
-    
-    
-    
+# def _convert_date_string_to_python_datetime(
+#         date_string
+#         ):
+#     ""
+#     return datetime.strptime(date_string,'%d/%m/%Y')
 
 
 
+    
+# def get_next_code(code):
+#     """
+#     """
+#     with sqlite3.connect(_get_fp_database()) as conn:
+#         c = conn.cursor()
+#         query=f"""
+#             SELECT 
+#                 GEOGCD, OPER_DATE
+#             FROM
+#                 changes
+#             WHERE
+#                 GEOGCD_P = "{code}"
+#             """
+            
+#         result=[(x[0],_convert_date_string_to_python_datetime(x[1])) 
+#                  for x in c.execute(query).fetchall()]
+        
+#         if len(result)==0:
+            
+#             raise ValueError('Code does not have a next code')
+            
+#         elif len(result)>1:
+            
+#             raise Exception('...wasnt execting a result of more that one item here...???')
+            
+#         else:
+            
+#             return result[0]
+    
+
+# def get_latest_code(code):
+#     """
+#     """
+#     while True:
+        
+#         try:
+            
+#             next_code = get_next_code(code)
+            
+#         except ValueError:
+            
+#             return code
+        
+#         code = next_code
+
+
+# def get_parent_codes(
+#         code,
+#         status='live'  # 'live' or 'terminated' or none 
+#         ):
+#     """
+#     """
+   
+    
+#     with sqlite3.connect(_get_fp_database()) as conn:
+#         c = conn.cursor()
+        
+#         query=f"""
+#             SELECT 
+#                 PARENTCD
+#             FROM
+#                 changehistory
+#             WHERE
+#                 GEOGCD = "{code}"
+#             """
+                
+#         if not status is None:
+            
+#             query+=f""" 
+#                  AND STATUS="{status}"
+#                 """
+            
+#         result=[x[0] for x in c.execute(query).fetchall()]
+        
+#         return result
+    
+    
+# def get_child_codes(
+#         code,
+#         status='live'  # 'live' or 'terminated' or none 
+#         ):
+#     """
+#     """
+   
+    
+#     with sqlite3.connect(_get_fp_database()) as conn:
+#         c = conn.cursor()
+        
+#         query=f"""
+#             SELECT 
+#                 GEOGCD
+#             FROM
+#                 changehistory
+#             WHERE
+#                 PARENTCD = "{code}"
+#             """
+                
+#         if not status is None:
+            
+#             query+=f""" 
+#                  AND STATUS="{status}"
+#                 """
+            
+#         result=[x[0] for x in c.execute(query).fetchall()]
+        
+#         return result
+    
+    
+# def get_ancestor_codes(
+#         code,
+#         status='live'  # 'live' or 'terminated' or none 
+#         ):
+#     """
+#     """
+#     result=set()
+    
+#     def _get_parent_codes(
+#             result,
+#             codes,
+#             status):
+        
+#         for code in codes:
+            
+#             x=get_parent_codes(code,status)  # a list of codes
+            
+#             result.update(x)
+            
+#             result = _get_parent_codes(result,x,status)
+            
+#         return result
+    
+#     result =  _get_parent_codes(result,[code],status)
+    
+#     return result
+
+
+# def get_descendent_codes(
+#         code,
+#         status='live'  # 'live' or 'terminated' or none 
+#         ):
+#     """
+#     """
+#     result=set()
+    
+#     def _get_child_codes(
+#             result,
+#             codes,
+#             status):
+        
+#         for code in codes:
+            
+#             x=get_child_codes(code,status)  # a list of codes
+            
+#             result.update(x)
+            
+#             result = _get_parent_child(result,x,status)
+            
+#         return result
+    
+#     result =  _get_parent_codes(result,[code],status)
+    
+#     return result
 
 
 
--- tests/test_ogp_functions.py
+++ tests/test_ogp_functions.py
@@ -9,17 +9,200 @@ import unittest
 
 from ogp_functions import ogp_functions
 
+import datetime
+import json
+
+
+
+data=ogp_functions.load_data()
+
+#with open('data.json','w') as f:
+#    json.dump(data,f,indent=4)
+
+
+#o=ogp_functions.OGPdata()
+
 
 
 class TestOGPFunctions(unittest.TestCase):
     ""
     
-    def test_download_latest_data_files(self):
+    def _test_load_data(self):
         ""
         
-        ogp_functions.download_latest_data_files()
+        result=ogp_functions.load_data()
+        
+        print(len(result))
+        #print(result)
+        #print([result[x] for x in list(result.keys())[:]])
+        
+        
+    def _test_load_data2(self):
+         ""
+         
+         with open('data.json') as f:
+             result=json.load(f)
+    
+    def _test_download_latest_data_files(self):
+        ""
+        
+        #ogp_functions.download_latest_data_files()
     
     
+    def test_get_previous_codes(self):
+        ""
+        code='E06000063'
+        result=ogp_functions.get_previous_codes(
+            code,
+            data
+            )
+        #print(result)
+        
+        self.assertEqual(
+            set(result),
+            set(['E07000028','E07000026','E07000029'])
+            )
+        
+        
+    def test_get_next_codes(self):
+        ""
+        code='E07000026'
+        result=ogp_functions.get_next_codes(
+            code,
+            data
+            )
+        #print(result)
+        
+        self.assertEqual(
+            set(result),
+            set(['E06000063'])
+            )
+        
+        
+        
+    def test_get_latest_codes(self):
+        ""
+        code='E07000026'
+        result=ogp_functions.get_latest_codes(
+            code,
+            data
+            )
+        #print(result)
+        
+        self.assertEqual(
+            set(result),
+            set(['E06000063'])
+            )
+        
+        
+    def test_get_parent_codes(self):
+        ""
+        code='E00130460'
+        result=ogp_functions.get_parent_codes(
+            code,
+            data
+            )
+        self.assertEqual(
+            set(result),
+            set(['E01025710'])
+            )
+                        
+        
+    def test_get_child_codes(self):
+        ""
+        code='E01025710'
+        result=ogp_functions.get_child_codes(
+            code,
+            data
+            )
+        self.assertEqual(
+            set(result),
+            set(['E00130450', 'E00130457', 'E00130458', 'E00130460'])
+            )
+        
+        
+    def test_get_ancestor_codes(self):
+        ""
+        code='E00130460'
+        result=ogp_functions.get_ancestor_codes(
+            code,
+            data
+            )
+        self.assertEqual(
+            set(result),
+            set(['E10000018', 'E07000130', 'E02005354', 'E12000004', 'E92000001', 'E01025710'])
+            )
+        
+        
+    def test_get_descendent_codes(self):
+        ""
+        code='E02005354'
+        result=ogp_functions.get_descendent_codes(
+            code,
+            data
+            )
+        self.assertEqual(
+            set(result),
+            set(
+                ['E00130463', 'E00178480', 'E33016309', 'E00130442', 'E33014322', 
+                 'E01034004', 'E00130464', 'E00130466', 'E33016308', 'E01025708', 
+                 'E00130472', 'E00130452', 'E00130460', 'E00130451', 'E33016307', 
+                 'E01025714', 'E00130454', 'E00130462', 'E00173301', 'E00130467', 
+                 'E01034005', 'E00130465', 'E33014320', 'E33014321', 'E00130453', 
+                 'E00130479', 'E00173302', 'E00173303', 'E00178593', 'E00178581', 
+                 'E00130437', 'E00130455', 'E00178631', 'E01025711', 'E01025713', 
+                 'E00130444', 'E01025710', 'E00130458', 'E00130477', 'E00130478', 
+                 'E00130459', 'E33016310', 'E01025712', 'E00178546', 'E00130456', 
+                 'E00130476', 'E00130457', 'E00130450', 'E00130438', 'E00130461', 
+                 'E00130445']
+                )
+            )
+        
+        
+    def _test_Class(self):
+        
+        code='E06000063'
+        result=o.get_previous_codes(
+            code
+            )
+        print(result)
+        
+        code='E07000026'
+        result=o.get_next_codes(
+            code
+            )
+        print(result)
+        
+        code='E07000026'
+        result=o.get_latest_codes(
+            code
+            )
+        print(result)
+        
+        code='E00130460'
+        result=o.get_parent_codes(
+            code
+            )
+        print(result)
+        
+        code='E00130460'
+        result=o.get_ancestor_codes(
+            code
+            )
+        print(result)
+        
+        code='E01025710'
+        result=o.get_child_codes(
+            code
+            )
+        print(result)
+        
+        code='E02005354'
+        result=o.get_descendent_codes(
+            code
+            )
+        print(result)
+        
     
     
 if __name__=='__main__':
