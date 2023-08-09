b724665e061e2fe1636ed20017466c4dd86ad2e8
Steven Firth <s.k.firth@lboro.ac.uk>
2023-08-09 13:26:40 +0100

    updates

---
+++ ogp_functions/ogp_functions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 13:16:59 2023
+
+@author: cvskf
+"""
+
---
+++ tests/test_ogp_functions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  9 13:08:36 2023
+
+@author: cvskf
+"""
+
+import unittest
+
+import ogp_functions
